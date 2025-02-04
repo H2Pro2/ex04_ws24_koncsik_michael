32c8336f937cbd06441d6e2744fd6bea18d8352e
Michael Koncsik <michael.gothic@hotmail.com>
2025-02-04 16:46:11 +0100

    Bonus: Created a directory for the different lyrics files. Changed main.py so it would take lyrics from the separate files.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # Koncsik Michael
 
 import score
-import lyrics
+# import lyrics 
+
+filenames = ["Barbara_Streisand", "Bohemian_Rhapsody", "Lemon_Tree", "One", "Respect", "Stairway_to_Heaven"]
 
 def main():
-    print(score.score(lyrics.RESPECT))
+    for name in filenames:
+        file_path = f"lyrics_folder/{name}"
+        with open(file_path, "r") as file:
+            content = file.read()
+            print(f"The score for {name} is: {round(score.score(content), 2)}.")
 
 if __name__ == "__main__":
     main()
